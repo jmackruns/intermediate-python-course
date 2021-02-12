c4e7bda0539a1da11c1f91f19608c2513eb18c46
jmackruns <jmacklin2018@gmail.com>
2021-02-12 13:04:08 -0800

    Making it work again.

--- dice_roller.py
+++ dice_roller.py
@@ -1,13 +1,14 @@
-import random
-dice_rolls =2
-
 def main():
+  import random
+  dice_rolls = 2
   dice_sum = 0
   for i in range(0,dice_rolls):
-
-    roll = random.randint(1,6)
+    roll = random.randint(1, 6)
     dice_sum += roll
-    print(f'You rolled a {roll}')
-  print(f'You rolled a total of {dice_sum}')
+  if roll == 1:
+    print(f'You rolled a {roll}! Critical Fail!')
+  else:
+    print(f'you rolled a {roll}')
+  print (f'You rolled a total of {dice_sum}')
 if __name__== "__main__":
   main()
